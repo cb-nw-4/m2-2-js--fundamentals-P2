5cc87f28439567679f549c41ce7071bf791aca34
Doug Wright <dw@nof49.ca>
2020-09-23 12:00:09 -0400

    Moved __test__backup out of repo directory

--- __tests__backup/exercise-10.test.js
+++
@@ -1,9 +0,0 @@
-const allLong = require("../../../workshop/exercise-10");
-
-test("Exercise 10", () => {
-  expect(allLong(["Scott", "Bob", "Ric", "Jim"])).toBe(false);
-  expect(allLong(["Bob", "Ric", "Jim"])).toBe(false);
-  expect(
-    allLong(["Cyborg", "Robin", "Batman", "Superman", "Aquaman", "Flash"])
-  ).toBe(true);
-});
--- __tests__backup/exercise-4.test.js
+++
@@ -1,6 +0,0 @@
-const calculateAverage = require("../../../workshop/exercise-4");
-
-test("Exercise 4", () => {
-  expect(calculateAverage(76, 60, 83, 100, 78)).toBe(79);
-  expect(calculateAverage(3, 1, 1, 1, 1)).toBe(1);
-});
