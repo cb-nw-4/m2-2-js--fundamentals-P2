39f53655175fdafe2cafa2b987ecfa7ff388da59
“Winnie <chanwinnie.wc@gmail.com>
2020-09-26 10:31:22 -0400

    Exercise 2 completed

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -6,7 +6,9 @@
 //
 // Edit only the code between the lines (below)
 function getMax(num1, num2) {
-  // your code here...
+
+  return Math.max(num1, num2);
+
 }
 // -----------------------------------------------------------------
 // Edit only the code between the lines (above)
@@ -17,6 +19,7 @@ function getMax(num1, num2) {
 console.log(getMax(12, 43));
 
 // Create more test examples.
-
+console.log(getMax(0, -1));
+console.log(getMax(-21, -57));
 // This is needed for automated testing (more on that later)
 module.exports = getMax;
