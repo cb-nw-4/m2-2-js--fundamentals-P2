03212c00b2e712f0d3948f2831e65053c2d0f465
“Winnie <chanwinnie.wc@gmail.com>
2020-09-26 11:38:03 -0400

    Exercise 4 completed

--- __workshop/exercise-4.js
+++ __workshop/exercise-4.js
@@ -7,6 +7,8 @@
 // -----------------------------------------------------------------
 function calculateAverage(num1, num2, num3, num4, num5) {
   // code here
+  let ans2 = ((num1 + num2 + num3 + num4 + num5)/5);
+  return Math.round(ans2);
 }
 // -----------------------------------------------------------------
 // Edit only the code between the lines (above)
@@ -16,6 +18,8 @@ function calculateAverage(num1, num2, num3, num4, num5) {
 console.log(calculateAverage(3, 1, 1, 1, 1));
 
 // Create more test cases.
+console.log(calculateAverage(0, 60, 55, 50, 100));
+console.log(calculateAverage(100, 60, 100, 50, 100));
 
 // This is needed for automated testing (more on that later)
 module.exports = calculateAverage;
